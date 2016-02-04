7550e9925ca3febd0aa4b36b5b6ab75c99e318c7
Raghavendra <raghavr@thoughtworks.com>
2016-02-04 17:59:41 +0530

    Raghavendra, David | #1572 | Shifted results summary to table

--- speedtest.js
+++ speedtest.js
@@ -1,83 +1,73 @@
 function getMin(array) {
-            return Math.min.apply(null, array);
-        }
+    return Math.min.apply(null, array);
+}
 
-        function getMax(array) {
-            return Math.max.apply(null, array);
-        }
+function getMax(array) {
+    return Math.max.apply(null, array);
+}
 
-        function getAvg(array) {
-            return array.reduce(function(sum, a, i, ar) {
-                sum += a;
-                return i == ar.length - 1 ? (ar.length == 0 ? 0 : sum / ar.length) : sum
-            }, 0);
-        }
+function getAvg(array) {
+    return array.reduce(function(sum, a, i, ar) {
+        sum += a;
+        return i == ar.length - 1 ? (ar.length == 0 ? 0 : sum / ar.length) : sum
+    }, 0);
+}
 
-        var config = {
-            parallel: true,
-            serial: true,
-            files: [{
-                size: '1 kb',
-                path: "/1KB.txt",
-                runs: 25
-            }, {
-                size: '10 kb',
-                path: "/10KB.txt",
-                runs: 10
-            }, {
-                size: '100 kb',
-                path: "/100KB.txt",
-                runs: 2
-            }, {
-                size: '1 mb',
-                path: "/1MB.txt",
-                runs: 1
-            }]
-        };
+var config = {
+    parallel: true,
+    serial: true,
+    files: [{
+        size: '1',
+        path: "/1KB.txt",
+        runs: 25
+    }, {
+        size: '10',
+        path: "/10KB.txt",
+        runs: 10
+    }, {
+        size: '100',
+        path: "/100KB.txt",
+        runs: 2
+    }]
+};
 
-        function runTests() {
-            config.files.forEach(function(file, index) {
-                var timeTaken = [];
-                for (var i = 0; i < file.runs; i++) {
-                    var startTime = +new Date;
-                    fetch(file.path + '?' + (+new Date)).then(function() {
-
-                        document.getElementById('file' + (index + 1)).value++;
-
-                        var run = document.getElementById('runs' + (index + 1));
-                        run.innerHTML = parseInt(run.innerHTML) + 1;
-
-                        timeTaken.push(+new Date - startTime);
-                        document.getElementById('min' + (index + 1)).innerHTML = getMin(timeTaken);
-                        document.getElementById('max' + (index + 1)).innerHTML = getMax(timeTaken);
-                        document.getElementById('avg' + (index + 1)).innerHTML = getAvg(timeTaken);
-
-                    });
-                }
+function runTests() {
+    config.files.forEach(function(file, index) {
+        var timeTaken = [];
+        for (var i = 0; i < file.runs; i++) {
+            var startTime = +new Date;
+            fetch(file.path + '?' + (+new Date)).then(function() {
+                var row = document.getElementById(file.size);
+                var requests = parseInt(row.getElementsByClassName('requests')[0].innerHTML);
+                row.getElementsByClassName('requests')[0].innerHTML = ++requests;
+                timeTaken.push(+new Date - startTime);
+                console.log(timeTaken);
+                row.getElementsByClassName('min')[0].innerHTML = getMin(timeTaken);
+                row.getElementsByClassName('max')[0].innerHTML = getMax(timeTaken);
+                row.getElementsByClassName('avg')[0].innerHTML = getAvg(timeTaken);
+                row.getElementsByClassName('speed')[0].innerHTML = file.size * 8 / (getAvg(timeTaken) / 1000);
             });
         }
+    });
+}
 
-        function init() {
-            var html = '';
-            config.files.forEach(function(file, index) {
-                html += '<div class="card">' +
-                    '<div class="left">File size: ' + file.size + '</div>' +
-                    '<div class="right  "><span id="runs' + (index + 1) + '">0</span>/' + file.runs + '</div>' +
-                    '<progress  id="file' + (index + 1) + '" value="0" max="' + file.runs + '" class="progressBar"></progress>' +
-                    '<div class="clear"></div>' +
-                    '<div class="left">' +
-                    'Min: <span id="min' + (index + 1) + '">0</span>ms, Max: <span id="max' + (index + 1) + '">0</span>ms' +
-                    '</div>' +
-                    '<div class="right">' +
-                    'Avg: <span id="avg' + (index + 1) + '">0</span>ms' +
-                    '</div>' +
-                    '</div>'
-            });
-            document.getElementById('results').innerHTML = html;
+function init() {
+    config.files.forEach(function(file, index) {
+        var row = document.createElement('tr');
+        row.setAttribute('id', file.size);
+        row.innerHTML = '<td>' + file.size + ' KB</td>' +
+            '<td><progress id="" value="0" max="25" class="progressBar"></progress></td>' +
+            '<td class="requests">0</td>' +
+            '<td class="min">0</td>' +
+            '<td class="max">0</td>' +
+            '<td class="avg">0</td>' +
+            '<td class="speed">0</td>';
+        document.getElementById('results').appendChild(row);
+    });
 
-            document
-                .getElementById('run')
-                .addEventListener('click', runTests);
-        }
+    document
+        .getElementById('run')
+        .addEventListener('click', runTests);
+}
 
-        init();
+init();
